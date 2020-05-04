28d15fe19bdcf0c6600b383e63e24cb0bf74181f
Eslam <eslam.rezk96@hotmail.com>
2020-05-05 01:19:43 +0200

    creating new object for order items

--- sales_promotion/models/sales_promotion.py
+++ sales_promotion/models/sales_promotion.py
@@ -10,7 +10,9 @@ class SalesPromotion(models.Model):
 
     name = fields.Char(string='Name', required=True, copy=False, readonly=True, index=True,
                        default=lambda self: _('New'))
-    partner_id = fields.Many2one(comodel_name="res.partner", string="Customer", required=True, )
+    customer = fields.Char(string="Customer", required=True, )
+    order_date = fields.Datetime(string="Order Date", required=True, )
+    order_items_ids = fields.One2many(comodel_name="sales.items", inverse_name="order_id", string='Order Items')
 
     @api.model
     def create(self, vals):
@@ -18,3 +20,12 @@ class SalesPromotion(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('sales.promotion.sequence') or _('New')
         result = super(SalesPromotion, self).create(vals)
         return result
+
+
+class SalesItems(models.Model):
+    _name = 'sales.items'
+
+    order_id = fields.Many2one(comodel_name="sales.promotion", string="Order Items")
+    items = fields.Char(string="Items", required=True, )
+    order_qty = fields.Integer(string="Order Qty", required=True, )
+    unit_price = fields.Float(string="Unit Price",  required=True, )
