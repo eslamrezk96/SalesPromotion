08123d3d8ee63ad7e2ac69eb4053ea0ca230f230
Eslam <eslam.rezk96@hotmail.com>
2020-05-04 14:54:37 +0200

    edit manifest file

--- sales_promotion/__manifest__.py
+++ sales_promotion/__manifest__.py
@@ -1,24 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sales_promotion",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'name': "Sales Promotion",
+    'summary': """ Module for managing the sales """,
+    'author': "Eslam Mohamed Rezk",
+    'category': 'Sales',
+    'version': '12.0.1.0.0',
+    'installable': True,
+    'application': True,
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
