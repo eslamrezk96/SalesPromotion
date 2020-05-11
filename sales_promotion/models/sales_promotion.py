f63b72b2b45ae2545ecaaff7cafc3882cdfba15d
Eslam <eslam.rezk96@hotmail.com>
2020-05-12 01:24:16 +0200

    adding sales promotion

--- sales_promotion/models/sales_promotion.py
+++ sales_promotion/models/sales_promotion.py
@@ -13,7 +13,9 @@ class SalesPromotion(models.Model):
     customer = fields.Char(string="Customer", required=True, )
     order_date = fields.Datetime(string="Order Date", required=True, )
     order_items_ids = fields.One2many(comodel_name="sales.items", inverse_name="order_id", string='Order Items')
-    amount_untaxed = fields.Float(string='Untaxed Amount', readonly=True, compute='_amount_all', store=True)
+    total_with_out_dis = fields.Float(string='Total With Out Discount', readonly=True, compute='_amount_all_with_dis',
+                                      store=True)
+    amount_untaxed = fields.Float(string='Total After Discount', readonly=True, compute='_amount_all', store=True)
     tax = fields.Float(string='Tax', readonly=True, compute='_compute_tax', store=True)
     total = fields.Float(string='Total', readonly=True, compute='_compute_total', store=True)
 
@@ -23,6 +25,19 @@ class SalesPromotion(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('sales.promotion.sequence') or _('New')
         result = super(SalesPromotion, self).create(vals)
         return result
+
+    @api.depends('order_items_ids.price_unit','order_items_ids.order_qty')
+    def _amount_all_with_dis(self):
+        """
+              compute the all total of the order items with out discount
+        """
+        for order in self:
+            total_with_out_dis = 0.0
+            for line in order.order_items_ids:
+                total_with_out_dis += line.price_unit * line.order_qty
+            order.update({
+                'total_with_out_dis': total_with_out_dis,
+            })
 
     @api.depends('order_items_ids.price_subtotal')
     def _amount_all(self):
@@ -37,7 +52,6 @@ class SalesPromotion(models.Model):
                 'amount_untaxed': amount_untaxed,
             })
 
-
     @api.depends('amount_untaxed')
     def _compute_tax(self):
         """
@@ -46,10 +60,19 @@ class SalesPromotion(models.Model):
         for line in self:
             line.tax = (line.amount_untaxed * 14) / 100
 
-    @api.depends('amount_untaxed','total')
+    @api.depends('amount_untaxed', 'total')
     def _compute_total(self):
         for line in self:
             line.total = line.amount_untaxed + line.tax
+
+    @api.multi
+    def apply_promotion(self):
+        for order in self:
+            for line in order.order_items_ids:
+                if line.order_qty > 3:
+                    line.discount = ((line.price_unit * (-50)) / 100) * (line.order_qty - 3)
+                else:
+                    line.discount = 0.0
 
 
 class SalesItems(models.Model):
@@ -59,12 +82,13 @@ class SalesItems(models.Model):
     items = fields.Char(string="Items", required=True, )
     order_qty = fields.Integer(string="Order Qty", required=True, )
     price_unit = fields.Float('Unit Price', required=True, default=0.0)
+    discount = fields.Float('Discount', required=True, default=0.0)
     price_subtotal = fields.Float(compute='_compute_amount_subtotal', string='Subtotal', readonly=True, store=True)
 
-    @api.depends('order_qty', 'price_unit')
+    @api.depends('order_qty', 'price_unit', 'discount')
     def _compute_amount_subtotal(self):
         """
                 compute the subtotal of the order item
         """
         for lines in self:
-            lines.price_subtotal = lines.price_unit * lines.order_qty
+            lines.price_subtotal = (lines.price_unit * lines.order_qty) + lines.discount
