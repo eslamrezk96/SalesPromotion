58de65ee8d88aba9f4cc2e8694c3a74c1a5ef951
Eslam <eslam.rezk96@hotmail.com>
2020-06-22 16:58:26 +0200

    compelting sale promotion

--- sales_promotion/models/sales_promotion.py
+++ sales_promotion/models/sales_promotion.py
@@ -10,16 +10,6 @@ class SalesPromotion(models.Model):
 
     name = fields.Char(string='Name', required=True, copy=False, readonly=True, index=True,
                        default=lambda self: _('New'))
-    customer = fields.Char(string="Customer", required=True, )
-    promotion_date = fields.Datetime(string="Promotion Date", required=True, )
-
-    state = fields.Selection(string="Get Offer",
-                             selection=[('buy_x_get_y', 'Buy X Items and get Y free'),
-                                        ('get_disc', 'Discount'), ], )
-    items = fields.Char(string="Items", required=True, )
-    free_item = fields.Char(string="Free Items" , required=True,)
-    item_qty = fields.Integer(string="Items Qty", required=True, )
-    discount = fields.Float('Discount', required=True, default=0.0)
 
     @api.model
     def create(self, vals):
@@ -27,3 +17,33 @@ class SalesPromotion(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('sales.promotion.sequence') or _('New')
         result = super(SalesPromotion, self).create(vals)
         return result
+
+    customer = fields.Char(string="Customer", required=True, )
+    promotion_date = fields.Datetime(string="Promotion Date", required=True, )
+    sales_promotion_line_ids = fields.One2many(comodel_name="sales.promotion.line", inverse_name="sales_promotion_id",
+                                               string="Sales Promotion Line", )
+
+
+class SalesPromotionLine(models.Model):
+    _name = 'sales.promotion.line'
+
+    sales_promotion_id = fields.Many2one(comodel_name="sales.promotion", string="Sales Promotion", )
+    state = fields.Selection(string="Get Offer",
+                             selection=[('buy_x_get_y', 'Buy X Items and get Y free'),
+                                        ('get_disc', 'Discount'), ], )
+    items = fields.Many2one(comodel_name="items", string="Items", required=True, )
+    price_unit = fields.Float('Unit Price', required=True, default=0.0, related='items.price_item')
+    free_item = fields.Many2one(comodel_name="items", string="Free Items", )
+    item_qty = fields.Integer(string="Items Qty", required=True, )
+    discount = fields.Float('Discount %', required=True, default=0.0)
+    total = fields.Float('Total', required=True, default=0.0, compute='compute_total', sort=True)
+
+    @api.depends('price_unit', 'item_qty', 'discount')
+    def compute_total(self):
+        for lines in self:
+            if lines.state == 'buy_x_get_y':
+                lines.total = lines.price_unit * lines.item_qty
+            elif lines.state == 'get_disc':
+                sub_total = lines.price_unit * lines.item_qty
+                sub_discount = (sub_total * lines.discount) / 100
+                lines.total = sub_total - sub_discount
